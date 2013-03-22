52718278bb887992cb0c679b227979cb84dd70e5
johnmmcparland@gmail.com <johnmmcparland@gmail.com@a5cd9c44-5573-9ca1-8d90-f1208f326a5c>
2013-03-22 17:41:27 +0000

    All calculators implemented now.
    WebStorm files added.
    
    Removed duplicate app.js

--- trunk/src/main/webapp/app.js
+++
@@ -1,9 +0,0 @@
-Ext.application({
-    name: 'FMCoachRoles',
-    appFolder: 'app',
-    autoCreateViewport: true,
-    controllers: ['CalculatorController'],
-    launch: function() {
-     console.log("Launching FM Coach Roles");
-    }
-});
