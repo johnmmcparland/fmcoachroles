9115198ce1f95ba82c1e8394c909034e510ec9ec
johnmmcparland@gmail.com <johnmmcparland@gmail.com@a5cd9c44-5573-9ca1-8d90-f1208f326a5c>
2013-03-23 16:51:51 +0000

    Adding lgpl comments to every file I wrote.
    Adding the lgplv3 licence txt file to be distributed with the WAR.

--- src/main/webapp/app/view/CoachAssignmentChart.js
+++ src/main/webapp/app/view/CoachAssignmentChart.js
@@ -1,9 +1,19 @@
-/**
- * Created with JetBrains WebStorm.
- * User: t0135292
- * Date: 28/02/13
- * Time: 15:34
- * To change this template use File | Settings | File Templates.
+/*
+ * This file is part of FM Coach Roles.
+ * 
+ * FM Coach Roles is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FM Coach Roles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FM Coach Roles.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 Ext.require('Ext.chart.*');
 Ext.require('FMCoachRoles.store.AssignmentStore');
