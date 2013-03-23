931aa337095e405eeca35a24b52d02f19207d808
johnmmcparland@gmail.com <johnmmcparland@gmail.com@a5cd9c44-5573-9ca1-8d90-f1208f326a5c>
2013-03-23 13:09:58 +0000

    Got the result back from the server but so far cannot get it to update the store of the chart

--- trunk/src/main/webapp/app/controller/CalculatorController.js
+++ trunk/src/main/webapp/app/controller/CalculatorController.js
@@ -1,5 +1,11 @@
+Ext.require('FMCoachRoles.store.AssignmentStore');
+
 Ext.define('FMCoachRoles.controller.CalculatorController', {
     extend : 'Ext.app.Controller',
+
+    stores: ['AssignmentStore'],
+
+    // Initialize!
     init : function() {
         this.control({
             'nonPlayerBox button[action=calculate]' : {
@@ -8,33 +14,39 @@ Ext.define('FMCoachRoles.controller.CalculatorController', {
         });
     },
 
+    // Calculate
     calculate : function(button) {
         console.log("Calculating...");
         var form = button.up('form').getForm();// get the basic form
 
-        if (!form.isValid()) { // display error alert if the data is invalid
-            Ext.Msg.alert('Invalid Data', 'Please correct form errors.');
-        } else { // make sure the form contains valid data before
-            console.log("Form is valid: "+form);
+        if (form.isValid()) { // make sure the form contains valid data before
+            console.log("Form is valid: " + form);
+            form.add
             // submitting
             form.submit({
-            // success is based on the "success" property returned by
-            // CalculatorController.java
-			     success : function(form, action) {
-                     console.log(action);
-                     console.log(action.result);
-                     console.log(action.result.msg);
-                     Ext.Msg.alert('Success', action.result.msg);
-
-                 },
-            // failure is based on the "success" property returned by
-            // AuthenticationController.java
-                 failure : function(form, action) {
-                     console.log(action.result);
-                     var msg = action.result.msg;
-                     Ext.Msg.alert('Failed', msg);
+                // success is based on the "success" property returned by
+                // CalculatorController.java
+                success:function (form, action) {
+                    //console.log(action);
+                    console.log(action.result);
+                    loadAssignmentsData(action.result);
+                    //console.log(action.result.msg);
+                    Ext.Msg.alert('Success', 'Look at the recommended assignments');
+                },
+                // failure is based on the "success" property returned by
+                // AuthenticationController.java
+                failure:function (form, action) {
+                    console.log(action.result);
+                    var msg = action.result.msg;
+                    Ext.Msg.alert('Error', 'Sorry there was a problem determining the coach assignments');
                 }
             });
+        } else { // display error alert if the data is invalid
+            Ext.Msg.alert('Invalid Data', 'Please correct form errors.');
         }
+    },
+
+    loadAssignmentsData: function(result) {
+        this.getAssignmentStoreStore().loadData(result);
     }
 });
